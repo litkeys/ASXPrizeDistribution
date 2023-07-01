a55001ae1557044f23e2505bcdaa7b5e8abe2f7e
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-07-01 13:22:15 +1000

    Implemented dampen factor, which reduces the prize distribution gap

--- prizeCalculator.py
+++ prizeCalculator.py
@@ -1,6 +1,10 @@
 # the sample values in portfolio_values are from our winning league in 2023
 # $300 have been deducted from normal total_prize for extra arrangements
 
+# =======================
+dampen_factor = 0.8 # 1 is normal scale, 0.5 is square root, etc.
+# =======================
+
 total_prize = 2700 # the total prize received for winning
 
 minimum_prize = 100 # this is the prize received by the member with the lowest portfolio value
@@ -19,12 +23,11 @@ if minimum_prize * member_count > total_prize:
 
 bonus_pool = total_prize - minimum_prize * member_count
 
-max_deviation = sum(value - lowest_portfolio for value in member_portfolios)
+max_deviation = sum((value - lowest_portfolio)**dampen_factor for value in member_portfolios)
+deviation_ratios = [(value - lowest_portfolio)**dampen_factor / max_deviation for value in member_portfolios]
 
-deviation_ratios = [(value - lowest_portfolio) / max_deviation for value in member_portfolios]
-
-bonus_prizes = [minimum_prize + deviation_ratios[i] * bonus_pool for i in range(member_count)]
+distributed_prizes = [minimum_prize + deviation_ratios[i] * bonus_pool for i in range(member_count)]
 
 print("Below are the prizes (from low to high)")
-for prize in bonus_prizes:
+for prize in distributed_prizes:
     print(round(prize, 1))
