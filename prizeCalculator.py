ce9d0cdbc6ace3d32fbc8aae113307e407cc87e6
James Jiang <131339478+litkeys@users.noreply.github.com>
2023-07-01 13:09:15 +1000

    Run prizeCalculator.py for prize distributions
    Current values are real portfolio values from a league

---
+++ prizeCalculator.py
@@ -0,0 +1,30 @@
+# the sample values in portfolio_values are from our winning league in 2023
+# $300 have been deducted from normal total_prize for extra arrangements
+
+total_prize = 2700 # the total prize received for winning
+
+minimum_prize = 100 # this is the prize received by the member with the lowest portfolio value
+
+with open("portfolio_values.txt") as t:
+    data = t.readlines()[1:]
+    member_portfolios = sorted(float(value) for value in data)
+    lowest_portfolio, highest_portfolio = member_portfolios[0], member_portfolios[-1]
+    member_count = len(member_portfolios)
+
+#print(member_portfolios)
+
+if minimum_prize * member_count > total_prize:
+    print("Minimum prize too high, distribution is not possible.")
+    quit()
+
+bonus_pool = total_prize - minimum_prize * member_count
+
+max_deviation = sum(value - lowest_portfolio for value in member_portfolios)
+
+deviation_ratios = [(value - lowest_portfolio) / max_deviation for value in member_portfolios]
+
+bonus_prizes = [minimum_prize + deviation_ratios[i] * bonus_pool for i in range(member_count)]
+
+print("Below are the prizes (from low to high)")
+for prize in bonus_prizes:
+    print(round(prize, 1))
